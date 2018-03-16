bc08ba63ec2b4e15198b3f2f11fde424d93327f0
stanleyguo222 <stanley222@outlook.com>
2018-03-16 02:40:58 -0700

    version 1.2

--- Test_xinetd/server_test-httpd.c
+++ Test_xinetd/server_test-httpd.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unsitd.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define MAX 4096
 
@@ -31,6 +34,8 @@ int main(int argc, const char* argv[])
 	FILE* fp;
 	//
 	int ich;
+	//文件类型
+	char* type;
 
 	//参数传递不全
 	if(2 != argc)
@@ -59,24 +64,52 @@ int main(int argc, const char* argv[])
 
 	//判断是不是 / 开头的路径
 	if('/' != path[0])
-		send_err();
+		send_err(400, "Bad Request", "Bad filename.");
 
-	file = path + 1;
+	file = path + 1;	// ‘/'之后是文件名
+	
+#if 1
+	FILE *tmp = fopen("/home/stanley/test-dir/log.i", "a");
+	if(NULL == tmp) {
+		perror("fopen log.i error");
+		exit(1);
+	}
+#endif
 	
 	if(stat(file, &sbuf) < 0)
-		send_err();
+		send_err(404, "Not Found", "File not found.");
 
 	fp = fopen(file, "r");
 	if(NULL == fp)
-		send_err();
+		send_err(403, "Forbidden", "File is protected.");
+
+	char* dot = strrchr(file, '.');
+	
+	if(NULL == dot) {
+		type = "text/plain; charset=utf-8";
+	} else if(0 == (strcmp(dot, ".html"))) {
+		type = "text/html; charset=utf-8";
+	} else if(0 == (strcmp(dot, ".jpg"))) {
+		type = "image/jpeg";
+	} else if(0 == (strcmp(dot, ".gif"))) {
+		type = "image/gif";
+	} else if(0 == (strcmp(dot, ".png"))) {
+		type = "image/png";
+	} else if(0 == (strcmp(dot, ".mp3"))) {
+		type = "audio/mpeg";
+	} else if(0 == (strcmp(dot, ".mp4"))) {
+		type = "video/mpeg4";
+	} else {
+		type = "text/plain; charset=iso-8859-1";
+	}
 
 	//根据不同类别的请求完成指定的回复
-	send_headers();
+	send_headers(type, sbuf.st_size);
 
 	while(EOF != (ich = getc(fp)))
 		putchar(ich);
 
-	fflush(stdout)
+	fflush(stdout);
 
 	fclose(fp);
 
